ae9a394e420309e31ebdf0e7e5826b70b156eee7
jonathanraya <jonathanraya79@gmail.com>
2024-01-12 12:23:29 -0600

    images on car show go full screen when clicked.

--- src/pages/CarShow.js
+++ src/pages/CarShow.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, useParams } from "react-router-dom";
 import "./CarShow.css";
 
@@ -6,13 +6,19 @@ const CarShow = ({ cars, deleteCar, currentUser }) => {
   const { id } = useParams();
   let currentCar = cars?.find((car) => car.id === +id);
 
+  const [fullscreen, setFullscreen] = useState(false)
+
+  const toggleFullscreen = () => {
+    setFullscreen(!fullscreen)
+  }
+
   return (
     <>
-      <div className="show-page-card">
+      <div className={`show-page-card ${fullscreen ? "fullscreen-img" : ""}`} onClick={toggleFullscreen}>
         <div className="show-car-details">
           <div className="detail-item-pic">
             <strong></strong>
-            <img className="img" src={currentCar.image} alt="Car Image" />
+            <img className={`img ${fullscreen ? "fullscreen-img" : ""}`} src={currentCar.image} alt="Car Image" onClick={toggleFullscreen} />
           </div>
           <div className="detail-item">
             <strong>Year:</strong> {currentCar.year}
@@ -36,21 +42,18 @@ const CarShow = ({ cars, deleteCar, currentUser }) => {
             <strong>Description:</strong> {currentCar.description}
           </div>
         </div>
-        <div className={`show-car-details-buttons ${currentUser.id !== currentCar.user_id && "disable-button"}`}>
-          <NavLink to={`/caredit/${currentCar.id}`}>
-            <button className="edit-btn">Edit Project</button>
-          </NavLink>
-          <NavLink to={"/carindex"}>
-            <button
-              onClick={() => deleteCar(currentCar.id)}
-              className="edit-btn"
-            >
-              Delete Project
-            </button>
-          </NavLink>
+        <div className={`show-car-details-buttons ${currentUser.id  !== currentCar.user_id ? "disable-button" : ""}`}>
+            <NavLink to={`/caredit/${currentCar.id}`}>
+              <button className="edit-btn">Edit Project</button>
+            </NavLink>
+            <NavLink to={"/carindex"}>
+              <button onClick={() => deleteCar(currentCar.id)} className="edit-btn">
+                Delete Project
+              </button>
+            </NavLink>
+          </div>
         </div>
-      </div>
-    </>
+        </>
   );
 };
 
