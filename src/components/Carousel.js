f62f4e70e2877a629c0e632dd8b9ff04bbea1836
Jackson Pruett <jacksonpruett@Jacksons-MBP.lan>
2024-01-12 17:09:34 -0500

    added mediaquery to carousel to be responsive on smaller screens

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,25 +1,21 @@
-import React, { useState } from 'react';
-import './Carouselstyles.css'
-import {
-  Carousel,
-  CarouselItem,
-  CarouselCaption,
-} from 'reactstrap';
+import React, { useState } from "react";
+import "./Carouselstyles.css";
+import { Carousel, CarouselItem, CarouselCaption } from "reactstrap";
 
 const items = [
   {
-    src: 'https://i.postimg.cc/MH11Bn5p/homepageimageupdated.png',
-    altText: 'Slide 1',
+    src: "https://i.postimg.cc/MH11Bn5p/homepageimageupdated.png",
+    altText: "Slide 1",
     key: 1,
   },
   {
-    src: 'https://r4.wallpaperflare.com/wallpaper/233/811/707/mazda-mx-5-miata-stance-wallpaper-89d0b89d210a4d6bb667b8df300196cd.jpg',
-    altText: 'Slide 2',
+    src: "https://r4.wallpaperflare.com/wallpaper/233/811/707/mazda-mx-5-miata-stance-wallpaper-89d0b89d210a4d6bb667b8df300196cd.jpg",
+    altText: "Slide 2",
     key: 2,
   },
   {
-    src: 'https://i.redd.it/j95yhp56byyb1.jpg',
-    altText: 'Slide 3',
+    src: "https://i.redd.it/j95yhp56byyb1.jpg",
+    altText: "Slide 3",
     key: 3,
   },
 ];
@@ -42,31 +38,35 @@ function Example(args) {
 
   const slides = items.map((item) => {
     return (
-      <CarouselItem
-        onExiting={() => setAnimating(true)}
-        onExited={() => setAnimating(false)}
-        key={item.src}
-      >
-        <div className='slide-img-cont'>
-        <img style={{ width: '100%', height: 'auto' }} src={item.src} alt={item.altText} />
-        </div>
-        <CarouselCaption
-          captionText={item.caption}
-          captionHeader={item.caption}
-        />
-      </CarouselItem>
+        <CarouselItem
+          onExiting={() => setAnimating(true)}
+          onExited={() => setAnimating(false)}
+          key={item.src}
+        >
+          <div className="slide-img-cont">
+            <img
+              style={{ width: "100%", height: "auto" }}
+              src={item.src}
+              alt={item.altText}
+            />
+          </div>
+          <CarouselCaption
+            captionText={item.caption}
+            captionHeader={item.caption}
+          />
+        </CarouselItem>
     );
   });
 
   return (
-    <Carousel
-    activeIndex={activeIndex}
-    next={next}
-    previous={previous}
-    {...args}
-    >
-      {slides}
-    </Carousel>
+      <Carousel
+        activeIndex={activeIndex}
+        next={next}
+        previous={previous}
+        {...args}
+      >
+        {slides}
+      </Carousel>
   );
 }
 
